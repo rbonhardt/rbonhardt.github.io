654ef52abdb7faf0b7b27ee4a426a64f03914631
Ryan Bonhardt <rbonhardt@gmail.com>
2014-09-23 15:33:58 -0400

    added completed quiz app

--- quiz/js/app.js
+++ quiz/js/app.js
@@ -1,14 +1,8 @@
-var score = 0;
-var progress = 0;
-var questionCount = 1;
-var arrPosition = 0;
-var selected = false;
-
 function Question(state, cities, capital) {
 	this.state = state;
 	this.cities = cities;
 	this.capital = capital;
-};
+}
 
 var alabama = new Question('Alabama', ['Montgomery', 'Mobile', 'Birmingham', 'Tuscaloosa'], 'Montgomery');
 
@@ -16,19 +10,66 @@ var florida = new Question('Florida', ['Orlando', 'Miami', 'Tampa', 'Tallahassee
 
 var georgia = new Question('Georgia', ['Macon', 'Atlanta', 'Savannah', 'Bainbridge'], 'Atlanta');
 
-var northCarolina = ('North Carolina', ['Wake Forest', 'Salem', 'Raleigh', 'Charlotte'], 'Raleigh');
+var northCarolina = new Question('North Carolina', ['Wake Forest', 'Salem', 'Raleigh', 'Charlotte'], 'Raleigh');
 
-var california = ('California', ['Los Angeles', 'Sacramento', 'San Francisco', 'San Diego'], 'Sacramento');
+var california = new Question('California', ['Los Angeles', 'Sacramento', 'San Francisco', 'San Diego'], 'Sacramento');
 
-var questions = [georgia, florida, alabama, northCarolina, california];
+var questions = [alabama, florida, georgia, northCarolina, california];
 
 $(document).ready(function() {
 
-	// on page load intro will show
-	// for now, add this when done and uncomment h1s css 
-	// doIntro();
+	// on pageload do intro();
+	doIntro();
+
+	var arrPosition = 0;
+	var score = 0;
+	var progress = 0;
+	var questionCount = 1;
 
 	loadQuestion(arrPosition);
+
+	// user selects an answer and it becomes selected
+	$('.list').on('click', 'li', function () {
+		// but first we have to make sure all other selecteds are clear
+		$('.list').find('li').removeClass('selected');
+		$(this).addClass('selected');
+		$('.checkout h1').hide();
+		$('.checkout button').show();
+	});
+
+	$('.checkout').on('click', 'button', function() {
+		score = checkAnswer(arrPosition, score, questionCount);
+		progress += 20;
+		questionCount += 1;
+		updateProgress(score, progress, questionCount);
+		if (arrPosition < 4) {
+			$('.checkout button').hide();
+			$('.checkout h1').show();
+			$('.list').find('li').removeClass('selected');
+			arrPosition+=1;
+			loadQuestion(arrPosition);
+		} else {
+			console.log("done");
+			$('#question-count').text('5');
+			// clear quiz section out
+			$('.checkout').hide();
+			$('.heading h2').hide();
+			$('.options').hide();
+			$('.answers').show();
+			$('.reset').show();
+			// and give score
+			console.log(score);
+			$('.heading h1').text("You scored " + score + "%");
+			$('.list').find('li').removeClass('selected');
+			// give link to start over
+		}
+	});
+
+	$('.reset').on('click', function() {
+		location.reload(true);
+	});
+
+
 
 });
 
@@ -37,30 +78,14 @@ function doIntro() {
     $(this).fadeOut(1500, function() {
       $('.step-2').fadeIn(1500, function() {
 		    $(this).fadeOut(1500, function() {
-	        $('.step-3').fadeIn(1500, function() {
-				    $(this).fadeOut(1500, function() {
-			        $('.step-4').fadeIn(1500, function() {
-						    $(this).fadeOut(1500, function() {
-					        $('.step-5').fadeIn(1500, function() {
-								    $(this).fadeOut(1500, function() {
-							        $('.step-6').fadeIn(1500, function() {
-										    $(this).fadeOut(1500, function() {
-									        $('.main').fadeIn(1500);
-										    });
-										  });
-								    });
-								  });
-						    });
-						  });
-				    });
-				  });
-		    });
+		    	$('.main').fadeIn(2500);
+	  		});
 		  });
     });
-  })
+  });
 }
 
-function loadQuestion() {
+function loadQuestion(arrPosition) {
 	// then user will be presented with a question
 	$("#capital").text(questions[arrPosition].state);
 	
@@ -71,53 +96,37 @@ function loadQuestion() {
 		console.log(i);
 		console.log(questions[arrPosition]);
 		console.log(questions[arrPosition].cities[i]);
-	};
+	}
 
+}
 
-	// user selects an answer and it becomes selected
-	$('.list').on('click', 'li', function () {
-		// but first we have to make sure all other selecteds are clear
-		$('.list').find('li').removeClass('selected');
-		$(this).addClass('selected');
-		selected = true;
-	});
+function checkAnswer(arrPosition, score, questionCount) {
+	var answer = $('.list').find('li.selected .item').text();
+	console.log("Answer: " + answer);
+	var capital = questions[arrPosition].capital;
+	console.log("Capital: " + capital);
+	if (answer === capital) {
+		$('#update h1').text('Correct!');
+		score += 20;
+		console.log('score: ' + score);
+		$('.answers .list-item-' + questionCount + ' .answer').text(answer + ' is the capital of ' + questions[arrPosition].state);
+		$('.answers .list-item-' + questionCount).addClass('correct');
+	} else {
+		$('#update h1').text('Wrong!');
+		console.log('score: ' + score);
+		$('.answers .list-item-' + questionCount + ' .answer').text(answer + ' is not the capital of ' + questions[arrPosition].state);
+		$('.answers .list-item-' + questionCount).addClass('wrong');
+	}
+	return score;
+}
 
-	// when user submits answer selected 
-	$('.checkout').on('click', function() {
-		// check to make sure an answer is selected
-		if (selected === true) {
-			// progress bar moves 20%
-			progress += 20;
-			questionCount += 1;
-			$('#question-count').text(questionCount);
-			$("#top-progress").attr("aria-valuenow", progress);
-			$("#top-progress").attr("style", "width: "+progress+"%;");
-			//answer is captured
-			var answer = $('.list').find('li.selected .item').text();
-			var capital = questions[arrPosition].capital;
-			// console.log(answer);
-			// console.log(capital);
-			// and class goes away
-			$('.list').find('li').removeClass('selected');
-			selected = false;
-			// and app checks if capital equals answer selected
-			if (capital === answer){
-				// if it does correct flashes
-				$('#update h1').text("Correct!");
-				// and score goes up 20 points
-				score += 20;
-				$('#score').text(score);
-				$("#score-bar").attr("aria-valuenow", score);
-				$("#score-bar").attr("style", "width: "+score+"%;");
-			} else {
-				$('#update h1').text("Wrong!");
-			};
-		} else {
-			$('#update h1').text("You have to select an answer first");
-		}
-		console.log(arrPosition);
-		arrPosition+=1;
-		console.log(arrPosition);
-		loadQuestion(arrPosition);
-	});
-};
+function updateProgress(score, progress, questionCount) {
+	$('#question-count').text(questionCount);
+	$("#top-progress").attr("aria-valuenow", progress);
+	$("#top-progress").attr("style", "width: "+progress+"%;");
+	$('#score').text(score);
+	$('#score-bar').attr('aria-valuenow', score);
+	$('#score-bar').attr('style', 'width: '+score+'%;');
+
+}
+
