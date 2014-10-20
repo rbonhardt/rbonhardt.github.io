b7216834ed0f9862f1105bdc11e09ad9e0b0ad9e
Ryan Bonhardt <rbonhardt@gmail.com>
2014-10-20 16:08:24 -0400

    finished off my projects page

--- _site/js/app.js
+++ _site/js/app.js
@@ -1,27 +1,27 @@
 $(document).ready( function() {
 	$('.links').on('click', '.h', function() {
-		$('.portfolio div').hide();
-		$('.portfolio div.h').show();
+		$('.portfolio a').hide();
+		$('.portfolio a.h').show();
 	});
 
 	$('.links').on('click', '.j', function() {
-		$('.portfolio div').hide();
-		$('.portfolio div.j').show();
+		$('.portfolio a').hide();
+		$('.portfolio a.j').show();
 	});
 
 	$('.links').on('click', '.cw', function() {
-		$('.portfolio div').hide();
-		$('.portfolio div.cw').show();
+		$('.portfolio a').hide();
+		$('.portfolio a.cw').show();
 	});
 
 	$('.links').on('click', '.cc', function() {
-		$('.portfolio div').hide();
-		$('.portfolio div.cc').show();
+		$('.portfolio a').hide();
+		$('.portfolio a.cc').show();
 	});
 
 	$('.links').on('click', '.g', function() {
-		$('.portfolio div').hide();
-		$('.portfolio div.g').show();
+		$('.portfolio a').hide();
+		$('.portfolio a.g').show();
 	});
 
 });
