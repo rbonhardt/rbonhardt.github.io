551193cf8e13607aa4bde7c9b485b22fcc74ca6a
Ryan Bonhardt <rbonhardt@gmail.com>
2014-10-20 16:08:45 -0400

    finished projects page javascript

---
+++ js/app.js
@@ -0,0 +1,27 @@
+$(document).ready( function() {
+	$('.links').on('click', '.h', function() {
+		$('.portfolio a').hide();
+		$('.portfolio a.h').show();
+	});
+
+	$('.links').on('click', '.j', function() {
+		$('.portfolio a').hide();
+		$('.portfolio a.j').show();
+	});
+
+	$('.links').on('click', '.cw', function() {
+		$('.portfolio a').hide();
+		$('.portfolio a.cw').show();
+	});
+
+	$('.links').on('click', '.cc', function() {
+		$('.portfolio a').hide();
+		$('.portfolio a.cc').show();
+	});
+
+	$('.links').on('click', '.g', function() {
+		$('.portfolio a').hide();
+		$('.portfolio a.g').show();
+	});
+
+});
