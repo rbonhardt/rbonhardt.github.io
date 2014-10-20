c44306d934fb51779987bb764519771c75ee9924
Ryan Bonhardt <rbonhardt@gmail.com>
2014-10-20 13:50:09 -0400

    added clickable links to top of app

--- _site/js/app.js
+++ _site/js/app.js
@@ -1,8 +1,27 @@
 $(document).ready( function() {
 	$('.links').on('click', '.h', function() {
-		console.log('clicked')
 		$('.portfolio div').hide();
 		$('.portfolio div.h').show();
+	});
+
+	$('.links').on('click', '.j', function() {
+		$('.portfolio div').hide();
+		$('.portfolio div.j').show();
+	});
+
+	$('.links').on('click', '.cw', function() {
+		$('.portfolio div').hide();
+		$('.portfolio div.cw').show();
+	});
+
+	$('.links').on('click', '.cc', function() {
+		$('.portfolio div').hide();
+		$('.portfolio div.cc').show();
+	});
+
+	$('.links').on('click', '.g', function() {
+		$('.portfolio div').hide();
+		$('.portfolio div.g').show();
 	});
 
 });
